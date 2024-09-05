334ccb677e5807ace65272ea0d0a7b2fe25b67a0
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2024-09-05 17:06:23 +0200

    Get user input on keystroke

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,19 +8,34 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var _enteredTitle = '';
+
+  void _saveTitleInput(String inputValue) {
+    _enteredTitle = inputValue;
+  }
+
   @override
-  Widget build(BuildContext context) => 
-    const Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        children: [
-          TextField(
-            maxLength: 50,
-            decoration: InputDecoration(
-              label: Text('Title'),
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: _saveTitleInput,
+              maxLength: 50,
+              decoration: const InputDecoration(
+                label: Text('Title'),
+              ),
             ),
-          )
-        ],
-      ),
-    );
+            Row(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      print(_enteredTitle);
+                    },
+                    child: const Text('Save Expense')),
+              ],
+            )
+          ],
+        ),
+      );
 }
